fa062f80643b16542984bf7a995be19dda5c7d4b
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-28 19:17:13 -0300

    figura estatica

---
+++ projTeste/venv/meuPrimeiroProjeto/cliente/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import person_list
+
+
+urlpatterns = [
+    path('list/', person_list),
+]
--- projTeste/venv/meuPrimeiroProjeto/cliente/views.py
+++ projTeste/venv/meuPrimeiroProjeto/cliente/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+def person_list(request):
+    return render(request, 'pessoa.html')
