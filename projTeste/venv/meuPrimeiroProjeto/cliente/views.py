9e83913d35d39f37e1c943648a00ca2dc701e1a5
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-28 21:52:07 -0300

    person_update

--- projTeste/venv/meuPrimeiroProjeto/cliente/views.py
+++ projTeste/venv/meuPrimeiroProjeto/cliente/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Person
+from .forms import PersonForm
 
 # Create your views here.
 def person_list(request):
-    return render(request, 'pessoa.html')
+    person = Person.objects.all()
+    return render(request, 'person.html', {'person': person})
+
+def person_new(request):
+    form = PersonForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+    return render(request, 'person_form.html', {'form': form})
+
+def person_update(request, id):
+    person = get_object_or_404(Person, pk=id)
+    form = PersonForm(request.POST or None, request.FILES or None, instance=person)
+
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+    return render(request, 'person_form.html', {'form': form})
